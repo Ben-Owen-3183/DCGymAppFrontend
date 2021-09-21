8e02e02699aaa7968ee047efdba24b6fc3ae8ac3
Ben <ben.owen3183@gmail.com>
2021-09-21 11:16:02 +0100

    edited android build stuff

--- shared/settings.js
+++ shared/settings.js
@@ -1,12 +1,13 @@
 
-
+////////////////ws_siteURL: 'ws://178.79.180.26:8001/ws/',
+////////////////siteUrl: 'http://178.79.180.26:8001',
 
 const Settings = {
-    siteUrl: 'http://178.79.180.26:8001',
     // siteUrl: 'http://192.168.43.167:8000',
-    homePage: 'Feed',
-    ws_siteURL: 'ws://178.79.180.26:8001/ws/',
     // ws_siteURL: 'ws://192.168.43.167:8000/ws/',
+    siteUrl: 'https://app.davidcorfield.co.uk',
+    ws_siteURL: 'ws://app.davidcorfield.co.uk/ws/',
+    homePage: 'Feed',
     debug: true
 }
 
