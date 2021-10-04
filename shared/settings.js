033c77b161792753a84e410d167b5995aa2e5e32
Ben <ben.owen3183@gmail.com>
2021-10-04 17:28:59 +0100

    switching back to live url

--- shared/settings.js
+++ shared/settings.js
@@ -3,10 +3,10 @@
 ////////////////siteUrl: 'http://178.79.180.26:8001',
 
 const Settings = {
-    siteUrl: 'http://192.168.43.167:8000',
-    ws_siteURL: 'ws://192.168.43.167:8000/ws/',
-    // siteUrl: 'https://app.davidcorfield.co.uk',
-    // ws_siteURL: 'wss://app.davidcorfield.co.uk/ws/',
+    // siteUrl: 'http://192.168.43.167:8000',
+    // ws_siteURL: 'ws://192.168.43.167:8000/ws/',
+    siteUrl: 'https://app.davidcorfield.co.uk',
+    ws_siteURL: 'wss://app.davidcorfield.co.uk/ws/',
     homePage: 'Feed',
     debug: true
 }
